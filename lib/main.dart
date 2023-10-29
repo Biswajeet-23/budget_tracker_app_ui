9b125d2fd9585cdac4e397abfc4bd0654a8dec89
Biswajeet Sahoo <2000biswajeet@gmail.com>
2023-10-29 09:32:59 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:budget_tracker_app_ui/pages/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RootApp(),
+    );
+  }
+}
